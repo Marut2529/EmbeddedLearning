93d803c2792e736e7e7b26a100412b6b9fe10499
Marut2529 <marutjasmine@hotmail.com>
2026-08-05 15:50:40 +0100

    Update calculator.c

---
+++ 01_C_Basics/calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int x=20;
+int y=10;
+printf("20+10=%d\n",x+y);
+printf("20-10=%d\n",x-y);   
+printf("20*10=%d\n",x*y);
+printf("20/10=%d\n",x/y);
+printf("20%10=%d\n",x%y);
+
+}
