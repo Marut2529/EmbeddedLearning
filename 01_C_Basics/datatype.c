d88308ed51652f03efebd63cee05aa0bba170a26
Marut2529 <marutjasmine@hotmail.com>
2026-08-05 14:14:11 +0100

    Create datatype.c

---
+++ 01_C_Basics/datatype.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+int age=40;
+float height=172.5;
+char grade='A';
+bool pass=true;
+printf("Age:%d\n",age);
+printf("Height:%.2f\n",height);
+printf("Grade:c\n",grade);
+printf("Pass=%d\n",pass);
+
+}
