01eaa905b17bc30c588a79a70e9389fc2ac892d5
Marut2529 <marutjasmine@hotmail.com>
2026-08-05 14:47:00 +0100

    Update student.c

---
+++ 01_C_Basics/student.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+char name[]="Marut";
+int age=40;
+float height=172.5;
+float weight=68.5;
+char grade='A';
+bool pass=true;
+printf("Name:%s\n",name);
+printf("Age:%d\n",age);
+printf("Height:%.2f\n",height);
+printf("Weight:%.2f\n",weight);
+printf("Grade:%c\n",grade);
+printf("Pass=%d\n",pass);
+
+
+}
+
+
+
+
+
+
