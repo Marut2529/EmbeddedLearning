d0d6fa376e6c3183e55fef82d367fc63a42a16a1
Marut2529 <marutjasmine@hotmail.com>
2026-08-06 15:04:57 +0100

    Update battery.c

--- 01_C_Basics/battery.c
+++ 01_C_Basics/battery.c
@@ -1,1 +1,27 @@
 
+//Assignment1
+#include<stdio.h>
+int main(void)
+{
+int battery=55;
+if(battery>=80&&battery<=100)
+{
+printf("Excellent\n");
+}
+else if(battery<=79&&battery>=50)
+{
+printf("Good\n");
+}
+else if(battery<=49&&battery>=20)
+{
+printf("LOW\n");
+}
+else if(battery<=19&&battery>=0)
+{
+printf("Critical\n");
+}
+else
+{
+printf("Wrong number try afgain!\n");
+}
+}
