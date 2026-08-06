7243e5c3ecf57015e07ab5d6b34a7d3a37cd503c
Marut2529 <marutjasmine@hotmail.com>
2026-08-06 14:23:16 +0100

    Update grade.c

--- 01_C_Basics/grade.c
+++ 01_C_Basics/grade.c
@@ -1,1 +1,26 @@
-
+//if,else if,else
+#include<stdio.h>
+int main(void){
+int score = 59;
+if(score>=90)
+{
+    printf("Grade A\n");
+}
+else if(score>=80)
+{
+    printf("Grade B\n");
+}
+else if(score>=70)
+{
+    printf("Grade C\n");
+}
+else if(score>=60)
+{
+    printf("Grade D\n");
+}
+else
+{
+    printf("Grade F\n");
+}
+return 0;
+}
