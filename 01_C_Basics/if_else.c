faef9d186c96df0e79de5c2552680cd635b6789e
Marut2529 <marutjasmine@hotmail.com>
2026-08-06 11:40:16 +0100

    Add fan control logic to if_else.c

--- 01_C_Basics/if_else.c
+++ 01_C_Basics/if_else.c
@@ -1,6 +1,12 @@
 # include<stdio.h>
-int main(){
-  printf("Test ");
+int main(void){
+int temp=35;
+  if(temp>35){
+    printf("FAN ON\n");}
+  else{
+    printf("FAN OFF\n");
+  }
+  return 0;
 }
 
 
