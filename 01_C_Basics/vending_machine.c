0ca6a609c1521f359ba24a8ae1a4cd38b4a15c65
Marut2529 <marutjasmine@hotmail.com>
2026-08-06 15:57:24 +0100

    Update vending_machine.c

--- 01_C_Basics/vending_machine.c
+++ 01_C_Basics/vending_machine.c
@@ -1,1 +1,22 @@
-
+//Assignment4
+#include<stdio.h>
+int main(void)
+{
+int money=25;
+if(money>=50)
+{
+    printf("coffee\n");
+}
+else if(money>=30&&money<=49)
+{
+    printf("Tea\n");
+}
+else if(money>=20&&money<=29)
+{
+    printf("Water\n");
+}
+else{
+    printf("not enough money\n");
+}
+return 0;
+}
