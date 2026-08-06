e257b0c90328e48aba5b92daf2f6afa46b7f95b8
Marut2529 <marutjasmine@hotmail.com>
2026-08-06 15:45:24 +0100

    Update fan_control.c

--- 01_C_Basics/fan_control.c
+++ 01_C_Basics/fan_control.c
@@ -1,1 +1,23 @@
-
+//Assignment3
+#include<stdio.h>
+int main(void)
+{
+int temp=79;
+if(temp<40)
+{
+    printf("Fan OFF\n");
+}
+else if(temp>=40&&temp<=59)
+{
+    printf("Fan LOW\n");
+}
+else if(temp>=60&&temp<=79)
+{
+    printf("Fan MEDIUM\n");
+}
+else
+{
+    printf("Fan HIGH\n");
+}
+return 0;
+}
