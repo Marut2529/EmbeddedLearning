0d5e249d2e2996a533dcd541b86186f2684e114a
Marut2529 <marutjasmine@hotmail.com>
2026-08-06 15:33:25 +0100

    Update login.c

--- 01_C_Basics/login.c
+++ 01_C_Basics/login.c
@@ -1,1 +1,15 @@
-
+//Assignment2
+#include<stdio.h>
+int main(void)
+{
+int password=1234;
+int login=1234;
+if(password==login){
+    printf("Access Granted\n");
+}
+else
+{
+    printf("Access Denied\n");
+}
+return 0;
+}
