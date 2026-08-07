e1f0f99a92606cdd38bc795dc672a52998f24b58
Marut2529 <marutjasmine@hotmail.com>
2026-08-07 14:24:08 +0100

    Update traffic_light.c

--- 01_C_Basics/traffic_light.c
+++ 01_C_Basics/traffic_light.c
@@ -1,1 +1,32 @@
+/*
+============================================================
+File:trffic_light.c
+Author:Marut Chanda
+Description:Learn about switc..case
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int light=1;
+    switch(light)
+    {
+        case 1:
+        printf("RED\n");
+        break;
 
+        case 2:
+        printf("YELLOW\n");
+        break;
+
+        case 3:
+        printf("GREEN\n");
+        break;
+
+        default:
+        printf("Please try again\n");
+       
+    }
+return 0;
+}
