a4a2508441d05773ac19a9500fcbaff37f646d44
Marut2529 <marutjasmine@hotmail.com>
2026-08-07 09:19:13 +0100

    Update hello.c

--- 01_C_Basics/hello.c
+++ 01_C_Basics/hello.c
@@ -1,3 +1,12 @@
+/*
+============================================================
+File:hello.c
+Author:Marut Chanda
+Description:Learn first c programming
+Created: Aug 2026
+============================================================
+*/
+
 #include<stdio.h>
 int main(void)
 {
