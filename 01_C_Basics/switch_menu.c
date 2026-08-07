dc674e8d74db82312ae5ab1be62b9fb49d0c6284
Marut2529 <marutjasmine@hotmail.com>
2026-08-07 12:49:55 +0100

    Update switch_menu.c

--- 01_C_Basics/switch_menu.c
+++ 01_C_Basics/switch_menu.c
@@ -1,1 +1,31 @@
+/*
+============================================================
+File:switch_menu.c
+Author:Marut Chanda
+Description:Learn about switc..case
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int menu=2;
+    switch(menu)
+    {
+        case 1:
+             printf("Coffee\n");
+             break;
 
+        case 2:
+            printf("Tea\n");
+            break;
+
+        case 3:
+             printf("Water\n");
+             break;
+
+        default:
+            printf("Invalid Menu\n");
+    }
+return 0;
+}
