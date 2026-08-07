8ef7c0d013146cfe682d116936ac7643bfd94153
Marut2529 <marutjasmine@hotmail.com>
2026-08-07 14:37:47 +0100

    Update day_of_week.c

--- 01_C_Basics/day_of_week.c
+++ 01_C_Basics/day_of_week.c
@@ -1,1 +1,49 @@
+/*
+============================================================
+File:day_of_week.c
+Author:Marut Chanda
+Description:Learn about switch..case
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+int day=4;
+    switch(day)
+    {
+        case 1:
+        printf("Monday\n");
+        break;
+
+        case 2:
+        printf("Tuesday\n");
+        break;
+
+        case 3:
+        printf("Wednesday\n");
+        break;
+
+        case 4:
+        printf("Thursday\n");
+        break;
+
+        case 5:
+        printf("Friday\n");
+        break;
+
+        case 6:
+        printf("Satturday\n");
+        break;
+
+        case 7:
+        printf("Sunday\n");
+        break;
+
+        default:
+        printf("Try again\n");
+
+    }
+    return 0;
+    }
 
