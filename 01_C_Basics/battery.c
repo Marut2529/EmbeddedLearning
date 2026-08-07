7bb0d36d50d6ca36d62e8d9314d87776d8eef5aa
Marut2529 <marutjasmine@hotmail.com>
2026-08-07 12:11:33 +0100

    Update battery.c

--- 01_C_Basics/battery.c
+++ 01_C_Basics/battery.c
@@ -1,4 +1,3 @@
-
 /*
 ============================================================
 File:battery.c
@@ -7,19 +6,29 @@ Description:learn how to use if else..if.
 Created: Aug 2026
 ============================================================
 */
-//Assignment1
+
 #include<stdio.h>
 int main(void)
 {
-int battery=49;
+int battery=90;
 if(battery>=80&&battery<=100)
+{
 printf("Excellent\n");
+}
 else if(battery<=79&&battery>=50)
+{
 printf("Good\n");
+}
 else if(battery<=49&&battery>=20)
+{
 printf("LOW\n");
+}
 else if(battery<=19&&battery>=0)
+{
 printf("Critical\n");
+}
 else
+{
 printf("Wrong number try afgain!\n");
+}
 }
