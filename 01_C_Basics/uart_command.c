c8ae648cb632e150df5af2adb7a98ea558642b8f
Marut2529 <marutjasmine@hotmail.com>
2026-08-07 15:41:21 +0100

    Update uart_command.c

--- 01_C_Basics/uart_command.c
+++ 01_C_Basics/uart_command.c
@@ -1,1 +1,41 @@
+/*
+============================================================
+File:uart_command.c
+Author:Marut Chanda
+Description:Learn about switch..case
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    char cmd='R';
+    switch(cmd)
+    {
+        case'R':
+        printf("Motor Right\n");
+        break;
 
+        case 'L':
+        printf("Motor Left\n");
+        break;
+
+        case 'S':
+        printf("STOP\n");
+        break;
+
+        case 'F':
+        printf("Forward\n");
+        break;
+
+        case 'B':
+        printf("Backward\n");
+        break;
+
+        default:
+        printf("Try again\n");
+
+    }
+return 0;
+
+}
