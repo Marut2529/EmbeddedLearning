50a8b9d15ceeea192cf88e5f27176300ed261ce4
Marut2529 <marutjasmine@hotmail.com>
2026-08-07 15:01:15 +0100

    Update remote_control.c

--- 01_C_Basics/remote_control.c
+++ 01_C_Basics/remote_control.c
@@ -1,1 +1,35 @@
+/*
+============================================================
+File:remote_control.c
+Author:Marut Chanda
+Description:Learn about switch..case
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+int button=2;
+    switch (button)
+    {
+        case 1:
+        printf("Turn TV On\n");
+        break;
 
+        case 2:
+        printf("Turn TV Off\n");
+        break;
+
+        case 3:
+        printf("Volume +\n");
+        break;
+
+        case 4:
+        printf("Volume -\n");
+        break;
+
+        default:
+        printf("Try again\n");
+    }
+return 0;
+}
