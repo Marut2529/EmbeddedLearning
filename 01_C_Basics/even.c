2800421d2a4db0b1a678c31977f3b0522b22c762
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:06:18 +0100

    Update even.c

--- 01_C_Basics/even.c
+++ 01_C_Basics/even.c
@@ -1,1 +1,19 @@
-
+/*
+============================================================
+File:even.c
+Author:Marut Chanda
+Description:learn how to use for loop to create even number.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    for(i=0;i<=20;i+=2)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+  
+}
