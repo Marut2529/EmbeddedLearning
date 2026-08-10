e8911df934b8466281eaa3467217ac6e4c5e2d3a
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:41:48 +0100

    Update password_retry.c

--- 01_C_Basics/password_retry.c
+++ 01_C_Basics/password_retry.c
@@ -1,1 +1,22 @@
+/*
+============================================================
+File:password_retry.c
+Author:Marut Chanda
+Description:learn how to use for loop.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+int i;
+for(i=1;i<=3;i++)
+{
+
+    printf("Attempt %d\n",i);
+}
+ printf("System Locked\n");
+ return 0;
+}
+
 
