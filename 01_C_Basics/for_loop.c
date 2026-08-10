c07816af6044c70fb6630f66bfe62aab7c8963d4
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 11:05:19 +0100

    Update for_loop.c

--- 01_C_Basics/for_loop.c
+++ 01_C_Basics/for_loop.c
@@ -1,1 +1,8 @@
-
+/*
+============================================================
+File:for_loop.c
+Author:Marut Chanda
+Description:learn how to use for loop
+Created: Aug 2026
+============================================================
+*/
