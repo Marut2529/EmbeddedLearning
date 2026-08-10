f767fe590b029fc51ebd1d7ca82ef9df4ba65913
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 11:50:40 +0100

    Update for_loop.c

--- 01_C_Basics/for_loop.c
+++ 01_C_Basics/for_loop.c
@@ -6,3 +6,11 @@ Description:learn how to use for loop
 Created: Aug 2026
 ============================================================
 */
+#include<stdio.h>
+int main(void)
+{
+for(int i=1;i<=10;i++){
+printf("%d\n",i);
+}
+return 0;
+}
