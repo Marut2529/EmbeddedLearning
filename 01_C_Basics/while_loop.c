d370615890c6a51f4bbd04fbb5aa02c50ef36ef1
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 10:37:16 +0100

    Update while_loop.c

--- 01_C_Basics/while_loop.c
+++ 01_C_Basics/while_loop.c
@@ -1,1 +1,18 @@
-
+/*
+============================================================
+File:while_loop.c
+Author:Marut Chanda
+Description:learn how to use while loop
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+int i=1;
+while(i<=5)
+{
+printf("%d\n",i);
+i++;
+}
+}
