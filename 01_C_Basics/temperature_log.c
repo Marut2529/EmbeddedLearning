43a59c38835975a331d5f2fc1a43cbd75520300f
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:28:34 +0100

    Update temperature_log.c

--- 01_C_Basics/temperature_log.c
+++ 01_C_Basics/temperature_log.c
@@ -1,1 +1,19 @@
+/*
+============================================================
+File:temperature_log.c
+Author:Marut Chanda
+Description:learn how to use for loop to show temperature.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+int temp=25;
+for(int i=1;i<=10;i++)
+{
+    printf("Reading %d :%d C\n",i,temp);
+}
+return 0;
+}
 
