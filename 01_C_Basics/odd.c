58caf1a68644f426a8768f18e740682a42b367f2
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:09:57 +0100

    Update odd.c

--- 01_C_Basics/odd.c
+++ 01_C_Basics/odd.c
@@ -1,1 +1,18 @@
-
+/*
+============================================================
+File:odd.c
+Author:Marut Chanda
+Description:learn how to use for loop to create odd number.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    for(i=1;i<=19;i+=2)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
