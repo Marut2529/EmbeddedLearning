74b8a5b2fcda73a27fa3fe64b0ad345a02751603
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:00:52 +0100

    Update countdown.c

--- 01_C_Basics/countdown.c
+++ 01_C_Basics/countdown.c
@@ -1,1 +1,18 @@
-
+/*
+============================================================
+File:countdown.c
+Author:Marut Chanda
+Description:learn how to use for loop to coutdown number.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    for(i=10;i>=0;i--)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
