b36106f82d55e7abadd951aa6fb7845d3cc0b31d
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:19:32 +0100

    Update led_blink.c

--- 01_C_Basics/led_blink.c
+++ 01_C_Basics/led_blink.c
@@ -1,1 +1,19 @@
+/*
+============================================================
+File:led_blink.c
+Author:Marut Chanda
+Description:learn how to use for loop to to programming LED Blink.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int i;
+for(i=0;i<=10;i++){
+    printf("LED ON\n");
+    printf("LED OFF\n");
+}
+return 0;
+}
 
