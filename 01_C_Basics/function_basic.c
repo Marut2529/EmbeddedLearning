13ad3c15c448c6a3610d9f5ece0486abe6cbacd2
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 15:41:43 +0100

    Update function_basic.c

--- 01_C_Basics/function_basic.c
+++ 01_C_Basics/function_basic.c
@@ -1,1 +1,18 @@
-
+/*
+============================================================
+File:function_basic.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void say_hello(void)
+{
+    printf("Hello Embedded Engineer!\n");
+}
+int main(void)
+{
+    say_hello();
+    return 0;
+}
