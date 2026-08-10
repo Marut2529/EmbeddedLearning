30e995b3bf3c5c204c2c0a0c702d180c8f4d7b07
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:50:56 +0100

    Update sensor_monitor.c

--- 01_C_Basics/sensor_monitor.c
+++ 01_C_Basics/sensor_monitor.c
@@ -1,1 +1,21 @@
 
+============================================================
+File:sensor_monitor.c
+Author:Marut Chanda
+Description:learn how to use for loop.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+int i;
+for(i=1;i<=5;i++)
+{
+printf("Reading Sensor...\n");
+printf("Temperature :25\n");
+printf("Humidity :50\n");
+printf("------------------------------\n");
+}
+return 0;
+}
