8ba9fc333743775d8aac0a8c27be09a1df1f34d4
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 16:03:00 +0100

    Update temperature.c

--- 01_C_Basics/temperature.c
+++ 01_C_Basics/temperature.c
@@ -1,1 +1,20 @@
-
+/*
+============================================================
+File:temperature.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int read_temperature(void)
+{
+    return 25;
+}
+int main(void)
+{
+    int temperature;
+    temperature= read_temperature();
+    printf("Temperature = %d C\n",temperature);
+return 0;
+}
