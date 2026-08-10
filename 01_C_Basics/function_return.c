dcb24b25437a59fb828d5b89caa6be8756b3cba3
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 15:56:27 +0100

    Update function_return.c

--- 01_C_Basics/function_return.c
+++ 01_C_Basics/function_return.c
@@ -1,1 +1,21 @@
+/*
+============================================================
+File:function_return.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int add(int a, int b)
+{
+    return a+b;
+}
+int main(void)
+{
+    int result;
+    result=add(10,20);
+    printf("Result=%d\n",result);
+    return 0;
 
+}
