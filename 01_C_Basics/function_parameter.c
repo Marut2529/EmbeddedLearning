73dc6f353d9d859da21b86bc24a77305ecc05283
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 15:50:19 +0100

    Update function_parameter.c

--- 01_C_Basics/function_parameter.c
+++ 01_C_Basics/function_parameter.c
@@ -1,1 +1,22 @@
+/*
+============================================================
+File:function_parameter.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void print_number(int number)
+{
+    printf("Number = %d\n",number);
+}
+int main(void)
+{
+print_number(10);
+print_number(20);
+print_number(30);
 
+return 0;
+
+}
