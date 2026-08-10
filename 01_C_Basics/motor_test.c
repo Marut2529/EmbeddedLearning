91ee7500e0b8e4d77a587ff932a4ffca82fe3ec3
Marut2529 <marutjasmine@hotmail.com>
2026-08-10 14:34:23 +0100

    Update motor_test.c

--- 01_C_Basics/motor_test.c
+++ 01_C_Basics/motor_test.c
@@ -1,1 +1,17 @@
-
+/*
+============================================================
+File:motor_test.c
+Author:Marut Chanda
+Description:learn how to use for loop to test motor.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{int i;
+for(i=1;i<=5;i++)
+{
+printf("Motor Test %d\n",i);
+}
+return 0;
+}
