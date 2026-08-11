5fca46212726bc442bd53e436935eb96b8d1b6a2
Marut2529 <marutjasmine@hotmail.com>
2026-08-11 16:02:52 +0100

    Implement battery status function
    Added a function to check and print battery status based on input value.

--- 01_C_Basics/battery_function.c
+++ 01_C_Basics/battery_function.c
@@ -1,1 +1,38 @@
-
+/*
+============================================================
+File:battery_function.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void battery_status(int battery)
+{ if(battery>=80&&battery<=100)
+{
+    printf("Excellent\n");
+}
+else if(battery>=50&&battery<=79)
+{
+    printf("Good\n");
+}
+else if(battery>=20&&battery<=49)
+{
+    printf("LOW\n");
+}
+else
+{
+    printf("Critical\n");
+}
+return 0;
+}
+int main(void)
+{
+    battery_status(109);
+    battery_status(100);
+    battery_status(95);
+    battery_status(77);
+    battery_status(42);
+    battery_status(25);
+    return 0;
+}
