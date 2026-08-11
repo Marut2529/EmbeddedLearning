43f806a1324e86f4bb65cfa02bf1767a9b58e738
Marut2529 <marutjasmine@hotmail.com>
2026-08-11 10:28:57 +0100

    Update fan_function.c

--- 01_C_Basics/fan_function.c
+++ 01_C_Basics/fan_function.c
@@ -1,1 +1,34 @@
-
+/*
+============================================================
+File:fan_function.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void fan_control(int temperature)
+{
+    if(temperature<30)
+    {
+        printf("Fan OFF\n");
+    }
+    else if(temperature<60)
+    {
+        printf("Fan LOW\n");
+    }
+    else if(temperature<80)
+    {
+        printf("Fan Medium\n");
+    }
+    else
+    {
+        printf("Fan High\n");
+    }
+}
+int main (void)
+{
+    int temperature=200;
+    fan_control(temperature);
+    return 0;
+}
