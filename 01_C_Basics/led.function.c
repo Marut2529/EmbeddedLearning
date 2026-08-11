791edf69cbafc86fa6855b5e7f50807bb68d6aee
Marut2529 <marutjasmine@hotmail.com>
2026-08-11 15:10:11 +0100

    Update led.function.c

--- 01_C_Basics/led.function.c
+++ 01_C_Basics/led.function.c
@@ -1,1 +1,25 @@
-
+/*
+============================================================
+File:led_function.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void led_control(int state)
+{
+    if(state==1)
+    {
+        printf("LED ON\n");
+    }
+    else
+    {
+        printf("LED OFF\n");
+    }
+}
+int main (void)
+{
+led_control(1);
+led_control(0);
+}
