46212f5f2d1a0482775262daa14d75366d9bb931
Marut2529 <marutjasmine@hotmail.com>
2026-08-12 11:05:43 +0100

    Update motor_control.c

--- 01_C_Basics/motor_control.c
+++ 01_C_Basics/motor_control.c
@@ -1,1 +1,36 @@
-
+/*
+============================================================
+File:motor_control.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void motor_control(int speed)
+{
+    if(speed==0)
+    {
+        printf("MOto OFF\n");
+    }
+    else if(speed>=1&&speed<=30)
+    {
+        printf("Motor LOW\n");
+    }
+    else if(speed>=31&&speed<=70)
+    {
+        printf("Motor Medium\n");
+    }
+    else
+    {
+        printf("Motor HIGH\n");
+    }
+}
+int main (void)
+{
+motor_control(0);
+motor_control(25);
+motor_control(50);
+motor_control(90);
+return 0;
+}
