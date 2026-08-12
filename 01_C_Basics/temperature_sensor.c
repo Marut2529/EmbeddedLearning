b140634aa74a7f408cfa459892fde0e91371b78f
Marut2529 <marutjasmine@hotmail.com>
2026-08-12 10:37:56 +0100

    Update temperature_sensor.c

--- 01_C_Basics/temperature_sensor.c
+++ 01_C_Basics/temperature_sensor.c
@@ -1,1 +1,21 @@
-
+/*
+============================================================
+File:temperature_sensor.c
+Author:Marut Chanda
+Description:learn how to use function.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+    int read_temperature(void)
+    {
+        int temperature=25;
+        return temperature;
+    }
+int main(void)
+{
+    int temperature;
+    temperature = read_temperature();
+    printf("Temperature = %d C\n", temperature);
+    return 0;
+}
