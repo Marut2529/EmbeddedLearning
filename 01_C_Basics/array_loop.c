7431d32cefdf55e63417e8ed3309c57ac90dbea9
Marut2529 <marutjasmine@hotmail.com>
2026-08-12 12:54:06 +0100

    Update array_loop.c

--- 01_C_Basics/array_loop.c
+++ 01_C_Basics/array_loop.c
@@ -1,1 +1,19 @@
 
+/*
+============================================================
+File:array_loop.c
+Author:Marut Chanda
+Description:Basic Array Example.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int temperature[5]={25,27,26,30,26};
+    for(int i=0;i<5;i++)
+    {
+        printf("Temperature[%d] = %d C\n", i, temperature[i]);
+    }
+    return 0;
+}
