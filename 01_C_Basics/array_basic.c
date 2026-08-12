edb896e48f3bda2c2fbdb8f5024e01dd2305d3e9
Marut2529 <marutjasmine@hotmail.com>
2026-08-12 12:22:49 +0100

    Update array_basic.c

--- 01_C_Basics/array_basic.c
+++ 01_C_Basics/array_basic.c
@@ -1,1 +1,21 @@
 
+/*
+============================================================
+File:array_basic.c
+Author:Marut Chanda
+Description:Basic Array Example
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int temperature[5]={25,27,26,30,28};
+    printf("Temperature 1 = %d C\n", temperature[0]);
+    printf("Temperature 1 = %d C\n", temperature[1]);
+    printf("Temperature 1 = %d C\n", temperature[2]);
+    printf("Temperature 1 = %d C\n", temperature[3]);
+    printf("Temperature 1 = %d C\n", temperature[4]);
+
+return 0;
+}
