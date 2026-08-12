b2d7cfc5109ce9f8fe3a51f97e491c598be52782
Marut2529 <marutjasmine@hotmail.com>
2026-08-12 14:02:55 +0100

    Update sensor_array.c

--- 01_C_Basics/sensor_array.c
+++ 01_C_Basics/sensor_array.c
@@ -1,4 +1,5 @@
 
+
 /*
 ============================================================
 File:sensor_array.c
@@ -10,10 +11,10 @@ Created: Aug 2026
 #include<stdio.h>
 int main(void)
 {
-    int temperature[5]={25,27,26,30,28};
-    for(int i=0;i<5;i++)
-    {
-        printf("Temperature[%d] = %d C\n", i, temperature[i]);
+    int temperature[10]={22,24,23,25,27,29,28,26,24,23};
+    for(int i=0;i<10;i++){
+        printf("Temperature[%d] = %d C\n",i,temperature[i]);
+    
     }
-    return 0;
+return 0;
 }
