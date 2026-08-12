8dce358580fc38217308bff928336b4b28baaf65
Marut2529 <marutjasmine@hotmail.com>
2026-08-12 13:51:19 +0100

    Update sensor_array.c

--- 01_C_Basics/sensor_array.c
+++ 01_C_Basics/sensor_array.c
@@ -1,1 +1,19 @@
 
+/*
+============================================================
+File:sensor_array.c
+Author:Marut Chanda
+Description:Basic Array Example.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main(void)
+{
+    int temperature[5]={25,27,26,30,28};
+    for(int i=0;i<5;i++)
+    {
+        printf("Temperature[%d] = %d C\n", i, temperature[i]);
+    }
+    return 0;
+}
