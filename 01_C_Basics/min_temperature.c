851c28d91966a392a1a4c3289f95ff85a4e32022
Marut2529 <marutjasmine@hotmail.com>
2026-08-13 13:56:02 +0100

    Update min_temperature.c

--- 01_C_Basics/min_temperature.c
+++ 01_C_Basics/min_temperature.c
@@ -1,1 +1,23 @@
+/*
+============================================================
+File:min_temperature.c
+Author:Marut Chanda
+Description:find minimum temperature from sensor array.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+    int temp[10]={22,24,23,25,27,29,28,26,24,23};
+    int min=temp[0];
+    for(int i=1;i<10;i++){
+        if(temp[i]<min){
+            min=temp[i];
+        }
+    }
+    printf("Minimum Temperature is %d  C\n",min);
+    return 0;
+
+}
 
