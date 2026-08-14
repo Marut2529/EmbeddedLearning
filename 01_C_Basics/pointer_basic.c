83cdeb24dfdbd8d4d865b6a897c9bf32e8785fb0
Marut2529 <marutjasmine@hotmail.com>
2026-08-14 12:26:15 +0100

    Update pointer_basic.c

--- 01_C_Basics/pointer_basic.c
+++ 01_C_Basics/pointer_basic.c
@@ -1,1 +1,20 @@
 
+/*
+============================================================
+File:pointer_basic.c
+Author:Marut Chanda
+Description:Poiter basic example.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+int value=100;
+int*ptr=&value;
+printf("Value = %d\n",value);
+printf("Value through pointer = %d\n",*ptr);
+printf("Address =%p\n",(void*)&value);
+
+
+}
