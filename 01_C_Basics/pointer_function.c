4af40ee19b0d3eba25a601b25e3363d3c533728a
Marut2529 <marutjasmine@hotmail.com>
2026-08-14 14:36:30 +0100

    Update pointer_function.c

--- 01_C_Basics/pointer_function.c
+++ 01_C_Basics/pointer_function.c
@@ -1,1 +1,23 @@
 
+/*
+============================================================
+File:pointer_function.c
+Author:Marut Chanda
+Description:function and pointer
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void increase_temperature(int *temp)
+{
+    *temp=30;
+}
+int main()
+{
+    int temp=25;
+    printf("Before = %d C\n",temp);
+    increase_temperature(&temp);
+    printf("After = %d C\n",temp);
+return 0;
+
+}
