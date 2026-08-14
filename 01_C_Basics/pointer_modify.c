a609bda0d3a0f4029cdfef8f8dabbad6b8da2635
Marut2529 <marutjasmine@hotmail.com>
2026-08-14 13:56:20 +0100

    Update pointer_modify.c

--- 01_C_Basics/pointer_modify.c
+++ 01_C_Basics/pointer_modify.c
@@ -1,1 +1,20 @@
 
+/*
+============================================================
+File:pointer_modify.c
+Author:Marut Chanda
+Description:Modify variable using pointer.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+ int value=50;
+ int *ptr=&value;
+
+ printf("Initial Value= %d\n",value);
+ *ptr=150;
+ printf("New value= %d\n",value);
+ return 0;
+}
