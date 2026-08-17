55c61b925aa2f85405c660536e88421e829b6d16
Marut2529 <marutjasmine@hotmail.com>
2026-08-17 12:15:48 +0100

    Update pointer_sensor.c

--- 01_C_Basics/pointer_sensor.c
+++ 01_C_Basics/pointer_sensor.c
@@ -1,1 +1,21 @@
+/*
+============================================================
+File:pointer_sensor.c
+Author:Marut Chanda
+Description:pointer+ sensor.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void set_temperature(int *temp, int new_temperature)
+{
+*temp=new_temperature;
+}
 
+int main(){
+int temperature =25;
+printf("Before = %d C\n",temperature);
+set_temperature(&temperature,35);
+printf("After = %d C\n",temperature);
+return 0;
+}
