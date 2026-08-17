9e78668084e3a5e0eb73eb6406095f51fc314887
Marut2529 <marutjasmine@hotmail.com>
2026-08-17 14:22:25 +0100

    Update pointer_max.c

--- 01_C_Basics/pointer_max.c
+++ 01_C_Basics/pointer_max.c
@@ -1,1 +1,32 @@
 
+/*
+============================================================
+File:pointer_max.c
+Author:Marut Chanda
+Description:Find maximum temperature.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void find_max(int temp[], int size, int *max)
+{
+    *max=temp[0];
+    for(int i=1;i<size;i++)
+    {
+        if(temp[i]>*max)
+        {
+            *max=temp[i];
+             
+        }
+    }
+  
+}
+
+int main()
+{
+ int temp[5]={22,29,25,31,27};
+ int *max;
+ find_max(temp, 5,&max);
+printf("Maximum = %d C\n", max);
+return 0;
+}
