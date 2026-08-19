97a852da2d09cbbdd2ff769d73a6acc4bf043392
Marut2529 <marutjasmine@hotmail.com>
2026-08-19 16:01:51 +0100

    Update sensor_manager.c

--- 01_C_Basics/sensor_manager.c
+++ 01_C_Basics/sensor_manager.c
@@ -1,1 +1,42 @@
 
+
+/*
+============================================================
+File:sensor_manager.c
+Author:Marut Chanda
+Description:Display sensor data using function and pointer to struct.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+struct Sensor
+{
+    int temperature;
+    int humidity;
+};
+
+void print_sensor(struct Sensor *sensor)
+{
+    for(int i=0;i<3;i++){
+    printf("Sensor %d\n",i+1);
+    printf("Temperature = %d C\n",sensor[i].temperature);
+    printf("Humidity = %d %%\n",sensor[i].humidity);
+    }
+
+}
+int main()
+{
+    struct Sensor sensor[3];
+
+    sensor[0].temperature=25;
+    sensor[0].humidity =60;
+
+    sensor[1].temperature=30;
+    sensor[1].humidity =70;
+
+    sensor[2].temperature=28;
+    sensor[2].humidity =65;
+  print_sensor(sensor);
+   return 0;
+ 
+}
