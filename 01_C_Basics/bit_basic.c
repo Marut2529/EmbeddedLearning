c89b5ef40de48d73c4865c3d05688f404bbdd9cf
Marut2529 <marutjasmine@hotmail.com>
2026-08-19 16:19:52 +0100

    Update bit_basic.c

--- 01_C_Basics/bit_basic.c
+++ 01_C_Basics/bit_basic.c
@@ -1,1 +1,19 @@
 
+/*
+============================================================
+File:bit_basic.c
+Author:Marut Chanda
+Description:Basic bitwise AND operation.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{   
+    unsigned char a=5;
+    unsigned char b=3;
+
+    unsigned char result= a & b;
+    printf("Result =%d\n",result);
+
+}
