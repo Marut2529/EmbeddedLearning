b036609593ee5966dd8c164b12e173f72a87b2d2
Marut2529 <marutjasmine@hotmail.com>
2026-08-19 10:40:20 +0100

    Update pointer_struct.c

--- 01_C_Basics/pointer_struct.c
+++ 01_C_Basics/pointer_struct.c
@@ -1,1 +1,29 @@
 
+/*
+============================================================
+File:pointer_struct.c
+Author:Marut Chanda
+Description:Access sensor data using pointer to struct.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+struct Sensor
+{
+    int temperature;
+    int humidity;
+};
+int main()
+{
+struct Sensor sensor1;
+
+sensor1.temperature =25;
+sensor1.humidity =60;
+
+struct Sensor *ptr=&sensor1;
+
+printf("Temperature =%d C\n",ptr->temperature);
+printf("Hunidity = %d %%\n",ptr->humidity);
+return 0;
+
+}
