e96c5eba92d64422a2deaac51720729638a7891f
Marut2529 <marutjasmine@hotmail.com>
2026-08-20 12:08:10 +0100

    Update bit_xor.c

--- 01_C_Basics/bit_xor.c
+++ 01_C_Basics/bit_xor.c
@@ -1,1 +1,18 @@
 
+/*
+============================================================
+File:bit_xor.c
+Author:Marut Chanda
+Description:Basic bitwise XOR operation.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+    unsigned char a = 5; //00000101
+    unsigned char b = 3; //00000011
+    unsigned char result= a^b;
+printf("Result = %d\n",result);
+return 0;
+}
