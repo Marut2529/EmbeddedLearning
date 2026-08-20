3def1c06ee02bae3aa29ce39afdc7da9b7f3c4b4
Marut2529 <marutjasmine@hotmail.com>
2026-08-20 13:58:52 +0100

    Update bit_set.c

--- 01_C_Basics/bit_set.c
+++ 01_C_Basics/bit_set.c
@@ -1,1 +1,18 @@
 
+/*
+============================================================
+File:bit_set.c
+Author:Marut Chanda
+Description:Basic set bit.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+    unsigned char reg=0; //00000000
+    reg = reg|(1<<3);    //00001000
+    printf("Register = %d\n",reg);
+    return 0;
+
+}
