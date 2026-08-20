a19d1c8269d9a5ad84349aa56111f273284089a1
Marut2529 <marutjasmine@hotmail.com>
2026-08-20 16:11:17 +0100

    Implement bit toggle functionality in bit_toggle.c
    This program toggles the 4th bit of an 8-bit register and prints the result after each toggle.

--- 01_C_Basics/bit_toggle.c
+++ 01_C_Basics/bit_toggle.c
@@ -1,1 +1,19 @@
 
+/*
+============================================================
+File:bit_toggle.c
+Author:Marut Chanda
+Description:Toggle Bit.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{ unsigned char reg = 0; //00000000
+  reg ^=(1<<3); //00001000
+  printf("After Toggle 1 = %d\n",reg);
+  reg ^=(1<<3); 
+  printf("After Toggle 2 = %d\n",reg);
+  return 0;
+  
+}
