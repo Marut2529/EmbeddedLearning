8238eefc7de3f2db8552720fd0f24d1f2d70907c
Marut2529 <marutjasmine@hotmail.com>
2026-08-20 12:21:10 +0100

    Add basic bit shift example in bit_shift.c

--- 01_C_Basics/bit_shift.c
+++ 01_C_Basics/bit_shift.c
@@ -1,1 +1,18 @@
 
+
+/*
+============================================================
+File:bit_shift.c
+Author:Marut Chanda
+Description:Basic bit shift.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+    unsigned char value= 1; //00000001
+    unsigned char result = value << 3;//00001000
+    printf("Result = %d\n", result);
+    return 0;
+}
