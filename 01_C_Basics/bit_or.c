20af208270850904ed5cf3dacd6767e87a4180af
Marut2529 <marutjasmine@hotmail.com>
2026-08-20 11:06:35 +0100

    Update bit_or.c

--- 01_C_Basics/bit_or.c
+++ 01_C_Basics/bit_or.c
@@ -1,1 +1,20 @@
 
+/*
+============================================================
+File:bit_or.c
+Author:Marut Chanda
+Description:Basic bitwise OR operation.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+    unsigned char a = 5; //00000101
+    unsigned char b = 3; //00000011
+    unsigned char result = a|b;
+
+    printf("result is a|b= %d\n", result);
+    return 0;
+
+}
