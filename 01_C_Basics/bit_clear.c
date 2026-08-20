43250d2f375a414c72e664db56280dbd5e7a209b
Marut2529 <marutjasmine@hotmail.com>
2026-08-20 14:09:46 +0100

    Update bit_clear.c

--- 01_C_Basics/bit_clear.c
+++ 01_C_Basics/bit_clear.c
@@ -1,1 +1,22 @@
 
+/*
+============================================================
+File:bit_clear.c
+Author:Marut Chanda
+Description:Basic clear bit.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+   unsigned char reg =0;
+   //Set Bit 3
+   reg |= (1<<3);//00001000
+   printf("After Set Bit 3 = %d\n", reg); 
+
+   //Clear Bit 3
+   reg &=~(1<<3);  //11110111
+   printf("After Clear Bit 3 = %d\n",reg);
+   return 0;
+}
