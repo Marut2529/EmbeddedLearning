ad074c4e3d0e921d027d8879b6c10ea2490449c4
Marut2529 <marutjasmine@hotmail.com>
2026-08-21 15:02:01 +0100

    Update gpio_register_binary.c

--- 01_C_Basics/gpio_register_binary.c
+++ 01_C_Basics/gpio_register_binary.c
@@ -1,1 +1,45 @@
 
+
+/*
+============================================================
+File:gpio_register_binary.c
+Author:Marut Chanda
+Description:using SET BIT, CLEAR BIT, TOGGLE,CHECK.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+void print_binary(unsigned char value)
+{
+for(int i = 7; i >=0; i--)
+{
+printf("%d", (value>> i) & 1);
+}
+}
+
+
+int main()
+{
+    unsigned char GPIO = 0;
+
+    print_binary(GPIO); //00000000
+    printf("\n");
+
+    GPIO |=(1<<3);
+    print_binary(GPIO); //00001000
+    printf("\n");
+
+    GPIO |=(1<<5);
+    print_binary(GPIO); //00101000
+    printf("\n");
+
+    GPIO &=~(1<<3);
+    print_binary(GPIO); //00100000
+    printf("\n");
+
+    GPIO &=~(1<<5);
+    print_binary(GPIO); //00000000
+    printf("\n");
+    return 0;
+}
+
