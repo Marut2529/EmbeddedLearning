fde9d5adcfc7a7746799d72afc07b647e695bc32
Marut2529 <marutjasmine@hotmail.com>
2026-08-21 12:34:27 +0100

    Update gpio_register.c

--- 01_C_Basics/gpio_register.c
+++ 01_C_Basics/gpio_register.c
@@ -1,1 +1,35 @@
 
+/*
+============================================================
+File:gpio_register.c
+Author:Marut Chanda
+Description:using SET BIT, CLEAR BIT, TOGGLE,CHECK.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{   
+    unsigned char GPIO = 0; //00000000
+    printf("Initial GPIO = %d\n",GPIO);
+
+    GPIO |= (1<<3); //00001000
+    printf("After Set Bit 3 = %d \n",GPIO);
+
+    GPIO |= (1<<5); //00101000
+    printf("After Set Bit 5 = %d \n",GPIO);
+
+    if(GPIO&(1<<3)){ 
+    printf(" Bit 3 is ON\n"); //00101000
+}
+else
+{
+    printf(" Bit 3 is OFF\n");
+}
+    GPIO &=~(1<<3); //00101000
+    printf("After Cleat Bit 3 = %d\n ",GPIO); //00100000
+
+    GPIO ^=(1<<5); //00100000
+    printf("After Toggle Bit 5 = %d\n ",GPIO);
+return 0;
+}
