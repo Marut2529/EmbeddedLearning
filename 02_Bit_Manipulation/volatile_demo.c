49454d015c1773bf9952d639dea6cf37729b9cb5
Marut2529 <marutjasmine@hotmail.com>
2026-08-21 15:50:52 +0100

    Update volatile_demo.c

--- 02_Bit_Manipulation/volatile_demo.c
+++ 02_Bit_Manipulation/volatile_demo.c
@@ -1,1 +1,18 @@
 
+/*
+============================================================
+File:volatile_demo.c
+Author:Marut Chanda
+Description:Basic demonstration of volatile variable.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{
+    volatile unsigned char GPIO = 0;
+    GPIO |= (1<<3);
+    printf("GPIO = %d\n",GPIO);
+    return 0;
+    
+}
