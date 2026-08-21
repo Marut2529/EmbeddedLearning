4c00c26505b8da295dd8cc3049b64581d413e26d
Marut2529 <marutjasmine@hotmail.com>
2026-08-21 10:01:42 +0100

    Update bit_check.c

--- 01_C_Basics/bit_check.c
+++ 01_C_Basics/bit_check.c
@@ -1,1 +1,22 @@
-
+/*
+============================================================
+File:bit_check.c
+Author:Marut Chanda
+Description:Check Bit.
+Created: Aug 2026
+============================================================
+*/
+#include<stdio.h>
+int main()
+{   
+  unsigned char reg = 8; //00001000
+    if(reg & (1<<3)) //00001000
+    {
+        printf("Bit is On\n");
+    }
+else
+{
+    printf("Bit 3 is OFF\n");
+}
+return 0;
+}
